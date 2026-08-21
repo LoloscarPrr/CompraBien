b17570cc47fab468d9e77d5feff00f1d9dcce8cc
LoloscarPrr <oscar.ignacio.urrutia@gmail.com>
2026-08-21 16:42:37 -0400

    fix: target Android SDK 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "cl.comprabien.app"
-    compileSdk = 37
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "cl.comprabien.app"
         minSdk = 24
-        targetSdk = 37
+        targetSdk = 36
         versionCode = 1
         versionName = "0.1.0"
 
