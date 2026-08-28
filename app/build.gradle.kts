f647e0ca38ff2cbe11634d7ee7e47918a150df2b
LoloscarPrr <oscar.ignacio.urrutia@gmail.com>
2026-08-28 06:41:48 -0400

    chore: bump CompraBien to v0.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "cl.comprabien.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.4.1"
+        versionCode = 6
+        versionName = "0.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
